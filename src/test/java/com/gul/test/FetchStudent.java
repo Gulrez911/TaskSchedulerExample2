7866ebe0a20574aa8875fba2165be119d9999d70
gulfa <gulfa@Gulrez>
2019-10-01 16:45:57 +0530

    log4j done

--- src/test/java/com/gul/test/FetchStudent.java
+++ src/test/java/com/gul/test/FetchStudent.java
@@ -2,6 +2,7 @@ package com.gul.test;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,23 +12,42 @@ import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.gul.config.JpaConfig;
 import com.gul.entity.Student;
+import com.gul.repo.StudentRepository;
 import com.kgate.service.StudentService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = JpaConfig.class)
+//@ContextConfiguration(locations = "classpath:appcontext.xml")
 @WebAppConfiguration
 public class FetchStudent {
 
 	@Autowired
 	StudentService service;
+	@Autowired
+	StudentRepository repo;
 
+	private static Logger log = Logger.getLogger(FetchStudent.class);
+
+//	@Test
+//	public void testAddDepartment() {
+//		
+//		List<Student> li = service.findAll();
+//		for (Student student : li) {
+//			System.out.println("List: " + student.getFname());
+//			System.out.println("List: " + student.getLname());
+//			System.out.println("List: " + student.getEmail());
+//		}
+//	}
 	@Test
-	public void testAddDepartment() {
-		List<Student> li = service.findAll();
-		for (Student student : li) {
-			System.out.println("List: " + student.getFname());
-			System.out.println("List: " + student.getLname());
-			System.out.println("List: " + student.getEmail());
+	public void logn() {
+
+		Student std = repo.findByEmailAndPassword("gulfarooqui1@gmail.com", "1234");
+		if (std == null) {
+			log.info("Invalid");
+			System.out.println("Invalid");
+		} else {
+			log.info("Successfully Login");
+			System.out.println("Successfully login");
 		}
 	}
 }
