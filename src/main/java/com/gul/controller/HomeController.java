60f3e799dcd10eb85c484b144d5b10b160780cd3
gulfa <gulfa@Gulrez>
2019-10-01 10:48:07 +0530

    office

--- src/main/java/com/gul/controller/HomeController.java
+++ src/main/java/com/gul/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.gul.controller;
 
 import java.util.TimeZone;
 
+import org.apache.log4j.Logger;
 import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +10,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+	private static final Logger logger = Logger.getLogger(HomeController.class);
+
 	@GetMapping("/")
 	public String view() {
-		TimeZone timeZone = TimeZone.getTimeZone("Asia/Kolkata");
-		String exp = "";
-		CronTrigger trigger = new CronTrigger("test",timeZone);
+		logger.info("method called");
+//		TimeZone timeZone = TimeZone.getTimeZone("Asia/Kolkata");
+//		String exp = " "+" "+" "+" ";
+//		String exp = "* * * * * ? *";
+//		CronTrigger trigger = new CronTrigger(exp, timeZone);
 		return "index";
 	}
 }
---
+++ src/main/java/com/gul/repo/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.gul.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gul.entity.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+}
